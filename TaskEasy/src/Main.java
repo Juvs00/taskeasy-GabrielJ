30f7acbca53133f04f86b5b3ce63b65b394737eb
tembiapo <paulo.jesus5@estudante.ifms.edu.br>
2026-05-07 09:11:28 -0400

    Adicionado menu inicial

--- TaskEasy/src/Main.java
+++ TaskEasy/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao sistema TaskEasy\n"
                 + "");
+
+        System.out.println("1 - Cadastrar tarefa\n");
+        System.out.println("2 - Listar tarefas");
     }
     
 }
